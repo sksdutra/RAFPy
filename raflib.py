d26caef8a82d97830db7942a021ae13aab2de250
Sandro K. S. Dutra <sksdutra@gmail.com>
2013-09-12 22:06:29 -0300

    RAFPy can unpack content

--- raflib.py
+++ raflib.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import sys
-import struct
+from sys import exit
+from struct import unpack
+from os import path, makedirs
+from zlib import decompress
 
 class RAFLib:
     def __init__(self, path_raf, path_raf_data):
@@ -10,61 +12,52 @@ class RAFLib:
             self.raf_data = open(path_raf_data, "rb")
         except IOError, error:
             print error
-            sys.exit()
-        # Interpret readed data in little-endian order
-        self.le_data = lambda data: struct.unpack("<I", data)[0]
+            exit()
+        self.le_data = lambda data: unpack("<I", data)[0]  # Interpret readed data in little-endian order
         # RAF Check
         self.magic_number = self.le_data(self.raf.read(4))
         if self.magic_number != 0x18be0ef0:
             print("[Error]%s is not a Riot Archive File(RAF)." % self.raf.name)
-            sys.exit()
+            exit()
         else:
-            # Getting general information
-            self.version = self.le_data(self.raf.read(4))
-            self.manager_index = self.le_data(self.raf.read(4))
+            self.version = self.le_data(self.raf.read(4))  # File version
+            self.manager_index = self.le_data(self.raf.read(4))  # Internal use number
             self.file_list_offset = self.le_data(self.raf.read(4))  # Offset where's stored file info
             self.path_list_offset = self.le_data(self.raf.read(4))  # Offset whre's stored path info
-            print "[Info]%s is as Riot Archive File %s." % (self.raf.name, self.version)
+            print "[Info]%s is as Riot Archive File(RAF) %s." % (self.raf.name, self.version)
 
     def __get_file_entries(self):
         print "[Info]File List Offset starting at: %s. Storing..." % (hex(self.file_list_offset))
-        # Working with the file entries
-        file_list_entries = []
         number_of_entries = self.le_data(self.raf.read(4))
-        for file_entry in range(number_of_entries):
-            file_path_hash = self.le_data(self.raf.read(4))
-            data_offset = self.le_data(self.raf.read(4))  # Offset of the file in raf.dat
-            data_size = self.le_data(self.raf.read(4))  # Size of the file in raf.dat
-            path_list_index = self.le_data(self.raf.read(4))
-            # A list of tuples where each tuple is a file entry
-            file_list_entries.append((file_path_hash, data_offset, data_size, path_list_index))
-        return file_list_entries
+        # List of [file_path_hash, data_offset, data_size, path_list_index]
+        file_entries_list = [[self.le_data(self.raf.read(4)) for file_entry in range(4)] for file_entries in range(number_of_entries)]
+        return file_entries_list
 
     def __get_path_list_entries(self):
         print "{Info]Path List Offset starting at: %s. Storing..." % (hex(self.path_list_offset))
-        # Working with the path list entries
-        path_list_entries = []
         path_list_size = self.le_data(self.raf.read(4))  # Total size of the path list (bytes)
         path_list_count = self.le_data(self.raf.read(4))  # Number of entries in path list
-        for path_entry in range(path_list_count):
-            path_offset = self.le_data(self.raf.read(4))  # Number of bytes from the start of path list to start of the string
-            path_length = self.le_data(self.raf.read(4))  # Size of the path string
-            path_list_entries.append((path_offset, path_length))
+        # List of [path_offset, path_length]
+        path_list_entries = [[self.le_data(self.raf.read(4)) for file_entry in range(2)] for file_entries in range(path_list_count)]
         return path_list_entries
 
-    def __get_paths_and_files(self):
-        pass
-
     def unpack_content(self):
-        file_entries = self.__get_file_entries()
+        file_entries = sorted(self.__get_file_entries(), key=lambda index: index[3]) #  Sort the file entries by path_index
         path_list_entries = self.__get_path_list_entries()
-        print path_list_entries
-
-        # code
-
+        print "[Info]Generating %s files." % len(path_list_entries)
+        path_list = [self.raf.read(size) for relative_offset, size in path_list_entries] # Creating a list of paths to unpack files
+        if len(path_list) == len(file_entries):
+            for entry in range(len(path_list)):
+                print "[Info]Generating: %s" % path.abspath(path_list[entry])
+                the_path, filename = path.split(path_list[entry])
+                if not path.exists(the_path): makedirs(the_path)  # Create directory if it doesn't exist
+                actual_file = open(path_list[entry][0:-1], "w+")  # Create related file in path
+                self.raf_data.seek(file_entries[entry][1], 0)  # Go to correct data_offset in raf.dat
+                raf_data = self.raf_data.read(file_entries[entry][2]) # Read the data_size in raf.dat
+                try: actual_file.write(decompress(raf_data))  # Trying to decompress using zlib and write in correct file
+                except: actual_file.write(raf_data)  # If can't decompress write as is
+        else:
+            print "[Error]An error has ocurred..."
+            sys.exit()
         self.raf.close()
         self.raf_data.close()
-
-if __name__ == "__main__":
-    raf_tool = RAFLib("Archive_203379600.raf", "Archive_203379600.raf.dat")
-    raf_tool.unpack_content()
--- rafpy.py
+++ rafpy.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
 
-import sys
+__author__ = "Ondinha"
+__version__ = 1
+
+from sys import argv, exit
 
 import raflib
 
 def help():
-    print "RAFPy\n-u or --unpack: Unpacks the content of RAF (Riot Archive File).\n" \
+    print "Usage: -u or --unpack: Unpacks the content of RAF (Riot Archive File).\n" \
           "Example: python rafpy.py -u Archive.raf Archive.raf.dat"
 
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        if sys.argv[1] == "-u" or sys.argv[1] == "--unpack": raflib.RAFLib(sys.argv[2], sys.argv[3]).extract_content()
-        else: help()
-    else: help()
+    print "RAFPy %s by %s" % (__version__, __author__)
+    if len(argv) == 4:
+        if argv[1] == "-u" or argv[1] == "--unpack":
+            raflib.RAFLib(argv[2], argv[3]).unpack_content()
+            exit()
+        else:
+            help()
+            exit()
+    else:
+        help()
+        exit()
